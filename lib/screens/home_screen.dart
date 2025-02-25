8c59d956ef6f45e36b05d583e0654c0f336e30f9
HoussaineBenDhieb <131673785+houssainebendhieb@users.noreply.github.com>
2025-02-25 11:44:04 +0100

    add getit shared prefrences , kol chy mrigle

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:get/get.dart';
@@ -20,58 +19,54 @@ class HomeScreen extends StatelessWidget {
       backgroundColor: Colors.yellow[200],
       body: Padding(
           padding: EdgeInsets.all(40),
-          child: GetX<ToDoController>(
-            builder: (_) {
-              return ListView.separated(
-                  itemBuilder: (ctx, index) => Slidable(
-                    endActionPane:ActionPane(motion: StretchMotion(),
-                     children:[
-                     SlidableAction(onPressed: (ff){},
-                     backgroundColor: Colors.red,
-                     icon: Icons.delete,
-                     borderRadius: BorderRadius.circular(9),
-                     
-
-
-                     )
-
-                     ] ) ,
-                    child: Container(
-                          width: double.infinity,
-                          height: 77,
-                          decoration: BoxDecoration(
+          child: GetX<ToDoController>(builder: (_) {
+            return ListView.separated(
+                itemBuilder: (ctx, index) => Slidable(
+                      endActionPane:
+                          ActionPane(motion: StretchMotion(), children: [
+                        SlidableAction(
+                          onPressed: (_) {},
+                          backgroundColor: Colors.red,
+                          icon: Icons.delete,
+                          borderRadius: BorderRadius.circular(9),
+                        )
+                      ]),
+                      child: Container(
+                        width: double.infinity,
+                        height: 77,
+                        decoration: BoxDecoration(
                             color: Colors.yellow[500],
-                            borderRadius: BorderRadius.circular(9)
-                          ),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            children: [
-                              Checkbox(value: toDoController.todo_list[index].isDone,
-                               onChanged: (newValue){
-                                toDoController.updateToDostatus(toDoController.todo_list[index].id);
-                               },
-                               ),
-                               SizedBox(
-                                width: 20,
-                               ),
-                               Text(
-                                toDoController.todo_list[index].title,
-                                style: TextStyle(
-                                  decoration: 
-                                  !toDoController.todo_list[index].isDone ? null:TextDecoration.lineThrough
-                                ),
-                               )
-                                  
-                            ],
-                          ),
+                            borderRadius: BorderRadius.circular(9)),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            Checkbox(
+                              value: toDoController.todo_list[index].isDone,
+                              onChanged: (newValue) {
+                                toDoController.updateToDostatus(
+                                    toDoController.todo_list[index].id);
+                              },
+                            ),
+                            SizedBox(
+                              width: 20,
+                            ),
+                            Text(
+                              toDoController.todo_list[index].title,
+                              style: TextStyle(
+                                  decoration:
+                                      !toDoController.todo_list[index].isDone
+                                          ? null
+                                          : TextDecoration.lineThrough),
+                            )
+                          ],
                         ),
-                  ),
-                  separatorBuilder: (ctx, index) => SizedBox(
-                        height: 20,
                       ),
-                  itemCount: toDoController.todo_list.length);
-            }
-          )),
+                    ),
+                separatorBuilder: (ctx, index) => SizedBox(
+                      height: 20,
+                    ),
+                itemCount: toDoController.todo_list.length);
+          })),
       floatingActionButton: FormDialog(),
     );
   }
